83b36bd0bd319abfe1b0399cb68b87878975df05
Camila <camila.ortolane29@gmail.com>
2023-10-06 09:27:19 -0400

    tentando commit assim

---
+++ TesteGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Digite um número");
+double num = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite outro numero");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+double resultado = num * num1;
+
+Console.WriteLine($"O resultado é {resultado}");
+
+Console.ReadKey();
