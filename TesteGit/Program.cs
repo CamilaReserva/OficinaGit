c407f4f6d752fba0244502343c468301580ffc3c
Camila <camila.ortolane29@gmail.com>
2023-10-06 09:38:22 -0400

    outro commit

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,6 +7,12 @@ double num1 = Convert.ToDouble(Console.ReadLine());
 
 double resultado = num * num1;
 
+if(resultado == 0) { Console.WriteLine("Número invalido"); }
+if(resultado == 1) { Console.WriteLine("Número invalido"); }
+if(resultado == 2) { Console.WriteLine("Número invalido"); }
+if(resultado == 10) { Console.WriteLine("Número invalido"); }
+
+
 Console.WriteLine($"O resultado é {resultado}");
 
 Console.ReadKey();
